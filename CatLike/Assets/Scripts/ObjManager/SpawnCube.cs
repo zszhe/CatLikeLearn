edb9f9ee963b83713a3259db5df7227e1d6b5aeb
zszhe <2940750679@qq.com>
2023-02-13 14:53:21 +0800

    ObjManager - 生成区域

--- CatLike/Assets/Scripts/ObjManager/SpawnCube.cs
+++ CatLike/Assets/Scripts/ObjManager/SpawnCube.cs
@@ -25,4 +25,11 @@ public class SpawnCube : SpawnZone
             return transform.TransformPoint(p);
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
 }
